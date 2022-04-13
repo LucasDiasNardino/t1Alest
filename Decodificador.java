05cdb3c8f9a25d60721b2f09b5af567fa3ed0ce6
Enzo-Neves <enzo.neves93@gmail.com>
2022-04-13 11:26:22 -0300

    melhoria no decodificador

--- Decodificador.java
+++ Decodificador.java
@@ -5,29 +5,22 @@ public class Decodificador {
     
     public static void algoritimoenzo() {
         String caso = "caso01";
+        String letratransformada = " ";
+        String letra = " ";
         try (BufferedReader br = new BufferedReader(new FileReader(caso))) {
             String line = br.readLine();// cria a variavel line que le a 1 linha que contem apenas variaveis
             List<abedecario> casos = new ArrayList<abedecario>();
-            String letratransformada = " ";
+           
             while (line != null) { // cria um while para rodar todo o programa
                 String[] vect = line.split(" ");
-                String letra = vect[0];
-                if(vect[1] != null){
-                 letratransformada = vect[1];
-                    }else{
-                        line = br.readLine();
-                    }
+                letra = vect[0]; 
+                letratransformada = vect[1];   
+                line = br.readLine();
                 abedecario ab = new abedecario(letra, letratransformada);
                 casos.add(ab);
-                line = br.readLine();
 
-            }
-
-            for (int i = 0; i < caso.length(); i++) {
-        //        if (casos.get(i).getLetra() == "a") {
-                //    escrever("saida1.txt", casos.get(i).getLetratransformada());
-          //      }
-            }
+         }
+           
 
         } catch (IOException e) {
             System.out.println("Error:" + e.getMessage());
